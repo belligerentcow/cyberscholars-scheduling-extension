8d8c42e8963b20f0646fdbc559e3aed2ceb0c0d6
jrreap <jaydonreap@gmail.com>
2020-01-27 13:22:57 -0500

    Score weighs time and week usage

--- fitness_tester.js
+++ fitness_tester.js
@@ -1,15 +1,48 @@
 //import _ from "./lodash-core.js";
 
 // Utility Functions
+/**
+ * Calculates the "bonus" score points to be added if the group is slotted in the afternoon
+ * 
+ * @param {Date} timestamp The Date object representing the timestamp for this slot
+ * @returns {number} The additional "bonus" points this group gets because of it's time slot 
+ */
+function calcuateTimeScore(timestamp) {
+    let score = 0
+
+    if (timestamp.getHours() >= 13) {
+        score += 2
+    }
+
+    return score
+}
 
 /**
  * Calculates a score for the passed in group based on the defined "fitness" function
  * 
- * @param {array} group The array of all the people assigned to this group
+ * @param {string[]} slot The timeslot with two groups, one for each week
+ * @param {Date} timestamp The Date object that represents the time slot for this group
  * @returns {number} The score for this particular group
  */
-function calculateScore(group) {
-    return Math.abs(4 - (group.length ** 2))
+function calculateScore(slot, timestamp) {
+    let score = 0
+
+    // Score the first group
+    if (slot[0].length > 0) {
+        score += Math.abs(4 - (group.length ** 2))
+        score += calcuateTimeScore(timestamp)
+    }
+
+    // Score the second group, and reward those groups with this second week utilized
+    if (slot[1].length > 0) {
+        score += Math.abs(4 - (group.length ** 2))
+        score += calcuateTimeScore(timestamp)
+        
+        // Extra points for using this second week slot
+        score += 2
+    }
+
+    return score
 }
 
 // Exports
@@ -28,37 +61,37 @@ function checkFitness(weekObject, possibleNumSlot) {
 
     Object.keys(weekObject).forEach(key => {
         slots.push(weekObject[key])
-        timestamps.push(new Date(key))
+
+        // Convert to Date object (allows us to properly handle timezones without funky math)
+        let date = new Date(Number(key))
+
+        timestamps.push(date)
     })
 
     let score = 0
 
     for(let i = 0; i < slots.length; i++) {
         // Week one's groups for this slot
-        score += calculateScore(slots[i][0])
-
-        // Week two's groups for this slot
-        score += calculateScore(slots[i][1])
+        score += calculateScore(slots[i], timestamps[i])
     }
 
     score += Math.abs(possibleNumSlot - slots.length)
+
+    console.log("RAW_DATES: " + timestamps)
 
     return score
 }
 
 // In VS code you can run this file with Node.JS, change this data to test different scenarios 
 
+let slots1 = {
+    1580147531000: [["Taco", "Fluff"], ["Puff"]],
+    1580143931000: [["Rufus"], ["Tuff"]]
+}
+
 let slots2 = {
     1579010400000: [["Taco", "Fluff"], []],
     1578906000000: [["Puff", "Rufus"], []],
-    1578920400000: [["Tuff"], []]
-}
-
-let slots1 = {
-    1579010400000: [["Taco"], []],
-    1578924000000: [["Fluff"], []],
-    1578906000000: [["Puff"], []],
-    1579003200000: [["Rufus"], []],
     1578920400000: [["Tuff"], []]
 }
 
