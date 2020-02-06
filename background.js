53aab953763e0b669c159783b44feae5f0499081
Micah Pope <mpope316@gmail.com>
2020-02-06 15:26:19 -0500

    Add export to CSV functionality

--- background.js
+++ background.js
@@ -7,12 +7,12 @@ function objToArray(obj) {
   return return_array;
 }
 
-function generateTable(data) {
+function generateTable(orig_data) {
   let table = "<table><tr><th>Day/Time</th><th>Week 1</th><th>Week 2</th></tr>";
+  let data = JSON.parse(JSON.stringify(orig_data));
   delete data.possNumSlots;
   data = objToArray(data).sort((a, b) => a[0] - b[0]);
   for (a_time in data) {
-    console.log(data[a_time][0]);
     let date_time = new Date(Number(data[a_time][0]));
     let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     let day_of_week = days[date_time.getDay()];
@@ -25,7 +25,6 @@ function generateTable(data) {
     let readable_time = day_of_week + " " + hour + ":" + minutes + " " + ampm;
     table += "<tr><td>"+readable_time+"</td>";
     for (a_week in data[a_time]) {
-      console.log(data[a_time][1][a_week]);
       table += "<td>"+data[a_time][1][a_week].toString().split(',').join(', ')+"</td>";
     }
     table += "</tr>"; 
@@ -34,10 +33,62 @@ function generateTable(data) {
   return table;
 }
 
+/**
+ * Flatten and stringify a multidimensional array, and replace all commas with semicolons (for CSV interpretation)
+ * 
+ * @param {array} arr The array to be processed
+ * @returns A flattened and stringified array
+ */
+function flattenAndStringify(arr) {
+  let merged = [].concat.apply([], arr);
+  let stringified = merged.toString();
+  return stringified.split(",").join("; ");
+}
+
+function convertToCSV(solution) {
+  let rows = [
+    ["Day/Time", "Week 1", "Week 2"]
+  ];
+  delete solution.possNumSlots;
+  solution = objToArray(solution);
+  solution = solution.sort((a, b) => a[0] - b[0]);
+  console.log(solution);
+  var last_day_of_week = "";
+
+  for (var slot in solution) {
+    let date_time = new Date(Number(solution[slot][0]));
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day_of_week = days[date_time.getDay()];
+    let hour = date_time.getUTCHours();
+    var ampm = hour >= 12 ? 'pm' : 'am';
+    hour = hour % 12;
+    hour = hour ? hour : 12; // the hour '0' should be '12'
+    let minutes = date_time.getUTCMinutes();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+
+    if (day_of_week == last_day_of_week) {
+      rows.push([hour + ":" + minutes + ampm, flattenAndStringify(solution[slot][1][0]), flattenAndStringify(solution[slot][1][1])]);
+    } else {
+      rows.push([day_of_week + " " + hour + ":" + minutes + ampm, flattenAndStringify(solution[slot][1][0]), flattenAndStringify(solution[slot][1][1])]);
+    }
+    last_day_of_week = day_of_week;
+  }
+
+  let csvContent = "data:text/csv;charset=utf-8," + rows.map(e => e.join(",")).join("\n");
+  var encodedUri = encodeURI(csvContent);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "meeting_schedule.csv");
+  document.body.appendChild(link); // Required for FF
+  link.click(); // This will download the data file
+}
+
+var current_solution = null;
+
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-  let data_grid = generateTable(response[0])
-  console.log(data_grid);
-  document.getElementById("combo").innerHTML = data_grid;
+  current_solution = response[0];
+  let tabled = generateTable(response[0])
+  document.getElementById("combo").innerHTML = tabled;
   sendResponse({farewell: "goodbye"});
 });
 
@@ -47,4 +98,9 @@ generate_button.onclick = function() {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
+}
+
+let export_button = document.getElementById('exportButton');
+export_button.onclick = function() {
+  convertToCSV(current_solution);
 }
